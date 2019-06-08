9b9d07d7e76a5fc0237fedf65fc6330b9cc3a19b
ivan1874 <ivan51673@gmail.com>
2019-06-08 18:08:20 +0300

    Make package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='PyFlex',
+    version='0.1',
+    packages=['pyflex'],
+    url='https://github.com/AcaplaStd/Pyflex',
+    license='',
+    keywords='python framework gui',
+    author='1Gregory',
+    author_email='',
+    description='GUI Framework'
+)
